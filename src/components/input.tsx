8d50ed74921b0421c3dedf44265534df3ee03128
DiogoWernek <diogowernek86@gmail.com>
2025-02-19 23:53:49 -0300

    Finalizado Front pagina home

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,6 +1,6 @@
 import { Mail } from 'lucide-react'
 
-import { ComponentProps } from "react"
+import type { ComponentProps } from 'react'
 
 // interface InputProps extends ComponentProps<'input'> {
 //   error?: boolean
@@ -8,11 +8,11 @@ import { ComponentProps } from "react"
 
 // export function Input({ error = false, ...props }: InputProps) {
 //   return (
-//     <div 
+//     <div
 //       data-error={error}
 //       className='group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger'
 //     >
-//       <span 
+//       <span
 //         className='text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger'
 //       >
 //         <Mail />
@@ -30,11 +30,11 @@ interface InputRootProps extends ComponentProps<'div'> {
   error?: boolean
 }
 
-export function InputRoot({ error=false, ...props }: InputRootProps) {
-  return(
-    <div 
+export function InputRoot({ error = false, ...props }: InputRootProps) {
+  return (
+    <div
       data-error={error}
-      className='group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger'
+      className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger"
       {...props}
     />
   )
@@ -43,21 +43,16 @@ export function InputRoot({ error=false, ...props }: InputRootProps) {
 interface InputIconProps extends ComponentProps<'span'> {}
 
 export function InputIcon(props: InputIconProps) {
-  return(
-      <span 
-        className='text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger'
-        {...props}
-      />
+  return (
+    <span
+      className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
+      {...props}
+    />
   )
 }
 
 interface InputFieldProps extends ComponentProps<'input'> {}
 
 export function InputField(props: InputFieldProps) {
-  return(
-    <input
-      className="flex-1 outline-0 placeholder-gray-400"
-      {...props}
-    />
-  )
+  return <input className="flex-1 outline-0 placeholder-gray-400" {...props} />
 }
